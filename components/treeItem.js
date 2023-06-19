b6547f58453e38dfde0fe693f17689517b78b84f
Sebastián Burgos <snburgos@uc.cl>
2023-06-19 01:26:30 -0400

    Added rectangle border

--- components/treeItem.js
+++ components/treeItem.js
@@ -1,17 +1,20 @@
 class TreeItem extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' }); // creamos shadow root
+    this.attachShadow({ mode: 'open' });
     this.render();
   }
 
   render() {
     this.shadowRoot.innerHTML = `
       <style>
-        :host { /* selecciona a la componente anfitriona del shadow dom en este casp es el tree component mismo*/
+        :host {
           display: block;
           background-color: #f2f2f2;
           padding: 10px;
+          border: 1px solid #ccc;
+          border-radius: 5px;
+          margin-bottom: 5px;
         }
 
         ul {
@@ -19,11 +22,11 @@ class TreeItem extends HTMLElement {
           padding-left: 25px;
         }
 
-        li > button { /* deja de mostrar todos los botones de los elementos */
+        li > button {
           display: none;
         }
 
-        li.has-children > button {/* mostramos solo si tienen hijos */
+        li.has-children > button {
           display: inline;
           margin-right: 5px;
         }
@@ -39,7 +42,7 @@ class TreeItem extends HTMLElement {
 
       <li>
         <button>+</button>
-        <span><slot></slot></span> <!-- span para que vaya al lado del boton y slot para traer el titulo -->
+        <span><slot></slot></span>
         <ul>
           <!-- Aquí van los elementos children -->
         </ul>
@@ -48,24 +51,20 @@ class TreeItem extends HTMLElement {
   }
 
   connectedCallback() {
-    // este evento se gatilla cuando el elemento es insertado en el DOM
-
-    const button = this.shadowRoot.querySelector('button'); // tenemos el botón del shadow dom
-    const ul = this.shadowRoot.querySelector('ul'); // tenemos la lista del shadow dom
-    const children = Array.from(this.children); // traemos la lista de hijos del light dom
+    const button = this.shadowRoot.querySelector('button');
+    const ul = this.shadowRoot.querySelector('ul');
+    const children = Array.from(this.children);
 
     if (children.length > 0) {
-      //  revisamos si tiene hijos
-      this.shadowRoot.querySelector('li').classList.add('has-children'); // lo marcamos como que tiene hijos
-      ul.append(...children); // agregamos los hijos que trajimos del light dom
-      ul.style.display = 'none'; // ocultamos la lista
+      this.shadowRoot.querySelector('li').classList.add('has-children');
+      ul.append(...children);
+      ul.style.display = 'none';
     }
 
     button.addEventListener('click', () => {
-      // evento del boton de click
-      const isExpanded = button.textContent === '-'; // esta expandido si su simbolo es -
-      button.textContent = isExpanded ? '+' : '-'; // cambiamos el simbolo
-      ul.style.display = isExpanded ? 'none' : 'block'; // cambiamos la visibilidad de la lista
+      const isExpanded = button.textContent === '-';
+      button.textContent = isExpanded ? '+' : '-';
+      ul.style.display = isExpanded ? 'none' : 'block';
     });
   }
 }
